19583fbb33f04d07601672d2e38181883c129f85
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-06-09 14:56:23 +0600

    Extra section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from '../Banner/Banner';
+import ExtraSection from '../ExtraSection/ExtraSection';
 
 const Home = () => {
 	return (
 		<div>
 			<Banner></Banner>
+			<ExtraSection></ExtraSection>
 		</div>
 	);
 };
