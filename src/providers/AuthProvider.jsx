42bb933126ba84d6a2ec81de7cacb0e5704563c0
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-06-10 20:33:06 +0600

    Custom hook added to use axios

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -12,6 +12,7 @@ import {
 	updateProfile,
 } from 'firebase/auth';
 import app from '../firebase/firebase.config';
+import axios from 'axios';
 
 export const AuthContext = createContext(null);
 
@@ -53,6 +54,20 @@ const AuthProvider = ({ children }) => {
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
 			setUser(currentUser);
+
+			// get and set token
+			if (currentUser) {
+				axios
+					.post('http://localhost:5000/jwt', {
+						email: currentUser.email,
+					})
+					.then((data) => {
+						localStorage.setItem('access-token', data.data.token);
+					});
+			} else {
+				localStorage.removeItem('access-token');
+			}
+
 			setLoading(false);
 		});
 
