3ec91d1a3ea8ac56194d817225d8977e1cd3e79a
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-06-12 16:22:37 +0600

    Popular class and instructor page added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,10 +1,31 @@
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 import 'react-awesome-slider/dist/styles.css';
+import anime from 'animejs/lib/anime.es.js';
+import './Banner.css';
 
 import slide1 from '../../../assets/images/slide1.jpg';
 import slide2 from '../../../assets/images/slide2.jpg';
 import slide4 from '../../../assets/images/slide4.jpg';
+
+anime
+	.timeline({ loop: true })
+	.add({
+		targets: '.ml2 .letter',
+		scale: [4, 1],
+		opacity: [0, 1],
+		translateZ: 0,
+		easing: 'easeOutExpo',
+		duration: 950,
+		delay: (el, i) => 70 * i,
+	})
+	.add({
+		targets: '.ml2',
+		opacity: 0,
+		duration: 1000,
+		easing: 'easeOutExpo',
+		delay: 1000,
+	});
 
 const Banner = () => {
 	return (
@@ -17,9 +38,10 @@ const Banner = () => {
 					<div className="absolute inset-0 bg-black opacity-50"></div>
 
 					<div className="z-10 text-center">
-						<h2 className="text-3xl md:text-7xl text-white mb-5">
-							Unlimited classes <br />
-							Unlimited learning!
+						<h2 className="text-3xl md:text-7xl text-white mb-5 ml2">
+							<span className="letter">Unlimited Classes</span>{' '}
+							<br />
+							<span className="letter">Unlimited learning!</span>
 						</h2>
 						<p className="text-white text-base md:text-2xl">
 							Start speaking confidently - with live classes{' '}
@@ -35,9 +57,14 @@ const Banner = () => {
 					<div className="absolute inset-0 bg-black opacity-50"></div>
 
 					<div className="z-10 text-center">
-						<h2 className="text-3xl md:text-7xl text-white mb-5">
-							Language Immersion Program <br />
-							Language-rich Environment!
+						<h2 className="text-3xl md:text-7xl text-white mb-5 ml2">
+							<span className="letter">
+								Language Immersion Program
+							</span>{' '}
+							<br />
+							<span className="letter">
+								Language-rich Environment!
+							</span>{' '}
 						</h2>
 						<p className="text-white text-base md:text-2xl">
 							Immerse yourself in a language-rich environment.
@@ -54,9 +81,15 @@ const Banner = () => {
 					<div className="absolute inset-0 bg-black opacity-50"></div>
 
 					<div className="z-10 text-center">
-						<h2 className="text-3xl md:text-7xl text-white mb-5">
-							Customized Language Courses <br />
-							Learn in a way you like!
+						<h2 className="text-3xl md:text-7xl text-white mb-5 ml2">
+							<br />
+							<span className="letter">
+								Customized Language Courses
+							</span>{' '}
+							<br />
+							<span className="letter">
+								Learn in a way you like!
+							</span>
 						</h2>
 						<p className="text-white text-base md:text-2xl">
 							Tailor your language learning experience with our
